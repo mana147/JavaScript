45d0c53ce027989aeb9e3bf9a93ee47f5ca16a0d
root <root@DESKTOP-GG5EUH7.localdomain>
2021-03-15 10:29:03 +0700

    update

--- js-advance/1_Callback_Higher_order_function.js
+++ js-advance/1_Callback_Higher_order_function.js
@@ -1,22 +1,40 @@
 /* 
-Khái niệm Callback và Higher-order function
+    ( In JavaScript, almost "everything" is an object )
+    Khái niệm Callback và Higher-order function:
 
-- Callback function : là hàm được truyền vào một hàm cha khác như một tham số đầu vào, 
-                    sau đó sẽ được gọi kích hoạt bên trong hàm cha này.
+    Tryền hàm A vào hàm B,
+    -   hàm A đc gọi là callback 
+    -   hàm B đc gọi là Higher-order-function 
 
-- Higher-order function: là hàm có hoạt động dựa trên 1 hàm khác, 
-                tức là: nó có thể nhận 1 hàm làm tham số đầu vào(hàm cha trong khái niệm callback function chính là một higher-order function), hoặc sẽ return ra 1 hàm khác. Một trong 2 điều kiện đó xảy ra thì được gọi là hàm higher-order.
+    * Callback function : (bản thân từ callback cũng cho chúng ta hiểu nôm na là được gọi và thực thi sau )
+        là hàm được truyền vào một hàm cha khác như một tham số đầu vào, 
+        sau đó sẽ được gọi kích hoạt bên trong hàm cha này.
 
+    * Higher-order function: 
+        là hàm có hoạt động dựa trên 1 hàm khác, tức là: nó có thể nhận 1 hàm làm tham số đầu vào
+        (hàm cha trong khái niệm callback function chính là một higher-order function), 
+        hoặc sẽ return ra 1 hàm khác. 
+        Một trong 2 điều kiện đó xảy ra thì được gọi là hàm higher-order.
 */
 
-function a(x) {
-    x++;
-    return function () {
-        console.log(++x);
-    };
+// hàm A đc gọi là hàm callback 
+function funcA () {
+    return 'hello'
+};
+// hàm B nhận 1 hàm làm tham số đầu vào
+// được gọi là hàm higher-order.
+function funcB(a) {
+    console.log(a());
 }
 
-let x = a();
-x();
-x();
-x();
+funcB(funcA);
+
+/*
+    Trong Js, nếu xét ở phía client, ngoài những đoạn code xử lí tuần tự thông thường, 
+    ta có 2 hoạt động tương đối khác biệt so với những ngôn ngữ server khác, đó là:
+        
+    - Lắng nghe event: lắng nghe sự kiện click chuột, bấm phím, hover .....
+        
+        - Xử lí bất đồng bộ: Đặc trưng nổi bật của JS là khả năng xử lí bất đồng bộ, 
+        có thể kể đến vài hoạt động như: gọi AJAX, đọc file dạng async, …
+*/ 
